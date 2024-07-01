21e9321f28843ef1ff3c5a675ac809fff204d18a
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-01 14:08:21 +0530

    OrOperator.java

---
+++ OrOperator.java
@@ -0,0 +1,13 @@
+public class OrOperator {
+    public static void main (String args [] ){
+        int a = 10 ;
+        int b = 5;
+        int c = 20;
+        System.out.println(a>b||a<c);
+        System.out.println(a>b|a<c);
+        System.out.println(a>b || a++ < c);
+        System.out.println(a);
+        System.out.println(a>b | a++ < c);
+        System.out.println(a);
+    }
+}
