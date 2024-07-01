894baaef7316b6bf42faf4a3e4d354852bee61f2
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-01 15:09:54 +0530

    OrOperator

--- OrOperator.java
+++ OrOperator.java
@@ -3,11 +3,12 @@ public class OrOperator {
         int a = 10 ;
         int b = 5;
         int c = 20;
-        System.out.println(a>b||a<c);
-        System.out.println(a>b|a<c);
-        System.out.println(a>b || a++ < c);
-        System.out.println(a);
-        System.out.println(a>b | a++ < c);
-        System.out.println(a);
+        System.out.println(a>b||a<c);//true || true = true 
+        System.out.println(a>b|a<c);//true | true = true  
+        //|| vs |  
+        System.out.println(a>b || a++ < c);//true || true = true 
+        System.out.println(a);//10 because second condition is not checked
+        System.out.println(a>b | a++ < c);//true | true = true  
+        System.out.println(a);//11 because second condition is checked 
     }
 }
