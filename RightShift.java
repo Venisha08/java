6526a90bac4e6ed9a963760b1f9bb341da88e136
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-01 12:10:27 +0530

    rightshift

---
+++ RightShift.java
@@ -0,0 +1,11 @@
+public class RightShift {
+    public static void main(String args[]){
+        System.out.println(10>>2);//10/2^2=10/4=2  
+        System.out.println(20>>2);//20/2^2=20/4=5  
+        System.out.println(20>>3);//20/2^3=20/8=2 
+        System.out.println(20>>>2);  
+        //For negative number, >>> changes parity bit (MSB) to 0  
+    System.out.println(-20>>2);  
+    System.out.println(-20>>>2);  
+    }
+}
