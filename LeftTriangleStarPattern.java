9d8a36591b01834eb9944e1c71ee9d11f4456895
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-02 17:27:59 +0530

    LeftTriangle

---
+++ LeftTriangleStarPattern.java
@@ -0,0 +1,19 @@
+public class LeftTriangleStarPattern {
+    public static void main(String[] args) {
+        //i for rows and j for columns      
+//row denotes the number of rows you want to print 
+int i ,j,row =6;
+        for(i=0;i<row;i++)
+        {
+            for(j=2*(row-i);j>=0;j--)
+            {
+                System.out.print(" ");
+            }
+            for (j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
