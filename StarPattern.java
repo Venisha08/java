d5f970b9d2af5e7cf16be850797b660db856c9e4
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-02 16:57:30 +0530

    RightTrianglePattern

---
+++ StarPattern.java
@@ -0,0 +1,16 @@
+public class StarPattern {
+    public static void main(String[] args) {
+        //i for rows and j for columns      
+//row denotes the number of rows you want to print  
+        int i,j,row =6;
+
+        for(i=0;i<row;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
