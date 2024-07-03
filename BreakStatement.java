2f1c770e452cb560e815fe8f87d13e59142b6a29
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-03 10:47:04 +0530

    odd

---
+++ BreakStatement.java
@@ -0,0 +1,10 @@
+public class BreakStatement {
+    public static void main(String[] args) {
+        int i =1 ;
+        System.out.println(" odd numbers:");
+        while(i<=50){
+            System.out.println(i);
+           i =i+2;
+        }
+    }
+}
---
+++ OddNumber.java
@@ -0,0 +1,12 @@
+public class OddNumber {
+        public static void main(String[] args) {
+            int i =1 ;
+            System.out.println(" odd numbers:");
+            while(i<=50){
+                System.out.println(i);
+               i =i+2;
+            }
+        }
+    }
+    
+
