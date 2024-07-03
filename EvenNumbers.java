0b1ef6895198981c0b9d9f0a75e6800115b84bbd
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-03 12:44:04 +0530

    do

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -1,8 +1,8 @@
 public class EvenNumbers {
     public static void main(String[] args) {
-        int i=0;{
+        int i=2;{
             System.out.println("First 10 even numbers:");
-            while(i<=10){
+            while(i<=100){
                 System.out.println(i);
                 i = i + 2;
             }
