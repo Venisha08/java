38b904e60429edb15cd294b1fc77dd19f8a2cff1
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-04 22:53:48 +0530

    num

---
+++ NumberPattern.java
@@ -0,0 +1,16 @@
+public class NumberPattern {
+   public static void main(String[] args) 
+   {
+     for(int row =1;row<=3;row++)
+     {
+
+     for(int col=1;col<=row;col++)
+     {
+        System.out.print(row);
+     }
+    System.out.println(" ");
+
+    }
+     }
+}
+    
