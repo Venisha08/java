fe587ee042c08aa47e1b540a20d6faf19b582f88
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-05 14:47:14 +0530

    Forloop.java

---
+++ StudentsMark.java
@@ -0,0 +1,19 @@
+public class StudentsMark {
+  public static void main(String[] args) {
+    int total =0;
+    for(int school =1;school<=10;school++){
+
+    for(int student=1;student <=5;student++)
+    {
+    for(int count =1;count<=5;count++){
+        int mark=90; 
+        total =total +mark;
+    }
+    System.out.println(total);
+    System.out.println(total/5);
+    
+    System.out.println("Total marks of the the student:");
+  }
+}
+}
+}
