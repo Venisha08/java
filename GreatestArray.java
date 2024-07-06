6f8c4211a3b04eb17b146b39829b2f0070a11adb
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-06 09:52:06 +0530

    Greatest number in array

---
+++ GreatestArray.java
@@ -0,0 +1,18 @@
+public class GreatestArray {
+    public static void main(String[] args) {
+        int[]no ={3,2,5};
+        int i =0;
+        int max=no[i];
+        while (i<no.length)
+         {
+            i++;
+            if(no[i]>max)
+         
+            {
+                max=no[i];
+            }
+        }
+
+
+    }
+}
