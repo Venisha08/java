576881e9296fc613dac97001d5ac323ed78b2969
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-10 17:04:09 +0530

    calculator

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public static void main(String[] args) {
+        Calculator casio=new Calculator();
+        casio.add();
+    }
+    private void add(){
+        int a =10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
