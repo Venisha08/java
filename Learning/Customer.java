56ba571b80a721745198a8cd48dbf47247528800
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-10 17:29:18 +0530

    Encapsulation method

---
+++ Learning/Customer.java
@@ -0,0 +1,9 @@
+package Learning;
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        bank manager=new bank();
+        manager.deposite();
+    }
+}
---
+++ Learning/bank.java
@@ -0,0 +1,17 @@
+package Learning;
+
+public class bank {
+    public int interest_Rate =4;
+    void deposite(){
+        System.out.println("Deposite");
+    }
+    private void fixInterestRate(){
+
+    }
+    private void withdraw(){
+        System.out.println("withdraw");
+    }
+    public void enquire(){
+        
+    }
+}
