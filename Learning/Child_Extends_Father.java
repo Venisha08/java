c59e574903ef6fa28212e4122d1c2dc088c9efb9
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-13 20:20:17 +0530

    using finalkeyword

---
+++ Learning/Child_Extends_Father.java
@@ -0,0 +1,11 @@
+package Learning;
+
+public class Child_Extends_Father {
+    
+    public static void main(String[] args) {
+        Child Childobj =new Child();
+        int amount = Childobj.Money;
+        Childobj.studyplan();
+
+    }
+}
---
+++ Learning/Father.java
@@ -0,0 +1,12 @@
+package Learning;
+
+public class Father {
+     final int Money =50;
+     void sing(){
+        System.out.println("Father is singing");
+     }
+     final void studyplan()
+     {
+        System.out.println("Father plan - no change");
+     }
+}
