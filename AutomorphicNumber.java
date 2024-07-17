f7b68feefa808bc8f09bce0d34150eb95b859d03
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-17 13:54:36 +0530

    square

---
+++ AutomorphicNumber.java
@@ -0,0 +1,18 @@
+public class AutomorphicNumber {
+     static boolean isAutomorphic(int num)
+     {
+        int square = num*num;
+        while(num > 0)
+        {
+            if(num % 10 != square %10)
+            return false;
+            num  = num/10;
+            square = square/10;
+        }
+        return true;
+     }
+     public static void main(String[] args) {
+        System.out.println(isAutomorphic(57)? "Automorphic": "Not Automorphic");
+        System.out.println(isAutomorphic(93)? "Automorphic":"Not Automorphic");
+     }
+    }
