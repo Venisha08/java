13e1e0bd1ccf67ab50282bf1e09a000fb188fcd5
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-18 19:30:46 +0530

    single inheritance

---
+++ Single_Inheritance/Venisha.java
@@ -0,0 +1,27 @@
+package Single_Inheritance;
+
+public class Venisha {
+    private int balance = 10000;
+    int doorNo =248;
+    protected String petname = "gowsi";
+    private void changeATM()
+    {
+
+    }
+    void gardening()
+    {
+
+    }
+    protected void DoYoga()
+    {
+
+    }
+    public  void act()
+    {
+        System.out.println("veni is acting");
+    }
+
+}
+
+    
+}
