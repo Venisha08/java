5d6ec5609f0a543b545c64d0f44ee8f0bbf93c67
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-20 20:55:10 +0530

    calling method using obj

---
+++ Single_Inheritance/Multilevel_Inheritance/Grandparent.java
@@ -0,0 +1,8 @@
+package Single_Inheritance.Multilevel_Inheritance;
+
+public class Grandparent {
+    void DoAgri()
+    {
+        System.out.println("Agri");
+    }
+}
---
+++ Single_Inheritance/Sanjay.java
@@ -0,0 +1,13 @@
+package Single_Inheritance;
+
+public class Sanjay extends Venisha 
+{
+
+    public static void main(String[] args) {
+        Sanjay Sanjayobj = new Sanjay();
+        Sanjayobj.act();
+        Sanjayobj.DoYoga();
+        Sanjayobj.gardening();
+    }
+    
+}
