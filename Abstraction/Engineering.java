71a20919d3c2882e400acdf188e55badf5231294
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-21 15:31:44 +0530

    One method abstract means whole class are abstract

---
+++ Abstraction/Engineering.java
@@ -0,0 +1,16 @@
+package Abstraction;
+
+abstract class Engineering 
+{
+    abstract void ClearAllExams();
+    {
+        void attendLab()
+        {
+
+        }
+        void attendclasses()
+        {
+
+        }
+    } 
+}
---
+++ Abstraction/Indians.java
@@ -0,0 +1,11 @@
+package Abstraction;
+
+abstract class Indians 
+{
+    int no;
+    abstract void havebreakfast();
+    void applyVoterID()
+    {
+        System.out.println("voterID");
+    }
+}
